10007c4f8f5fa641d716e8a3edac32853fdb58af
Vincenzo <vincent.luu@ynov.com>
2024-11-24 14:48:15 +0100

    boinus

--- bonus2Serveur.py
+++ bonus2Serveur.py
@@ -27,7 +27,6 @@ async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
     print(f"Nouvelle connexion de {addr}")
 
-    # Generate a random color for the client
     color = random.choice(COLORS)
 
     try:
