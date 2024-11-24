d79d002138b144a58ae6091a3da446f9d2e835f1
Vincenzo <vincent.luu@ynov.com>
2024-11-24 12:43:40 +0100

    tp6

--- chat_client_ii_5.py
+++ chat_client_ii_5.py
@@ -55,6 +55,10 @@ async def main():
             pass
 
 if __name__ == '__main__':
+    try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nClient arrêté manuellement.")
+
     
         
